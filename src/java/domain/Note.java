378ecf96f345299f1aab8cb7df70b90badf7a7a9
BrixBulil-lit <81497610+BrixBulil-lit@users.noreply.github.com>
2021-06-06 23:34:54 -0600

    Second commit Edited JavaBean to add 2 Strings (title & contents) Added a zero argument constructor and default constructor Added getters and setters

--- src/java/domain/Note.java
+++ src/java/domain/Note.java
@@ -21,6 +21,27 @@ public class Note implements Serializable {
         this.title = title;
         this.contents = contents;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" + "title=" + title + ", contents=" + contents + '}';
+    }
     
     
     
